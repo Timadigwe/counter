00c867a09898eb7e73fbe1e45d57c60930ddda00
timadigwe <timothyadigwe2019@gmail.com>
2023-11-30 22:25:54 +0100

    deployed

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,7 @@ pub enum Error {
 }
 
 /// Init function that creates a new smart contract.
-#[init(contract = "counter")]
+#[init(contract = "counter_2")]
 fn init(_ctx: &InitContext, _state_builder: &mut StateBuilder) -> InitResult<State> {
     // Your code
 
@@ -36,7 +36,7 @@ fn init(_ctx: &InitContext, _state_builder: &mut StateBuilder) -> InitResult<Sta
 pub type IncrementVal = i8;
 
 #[receive(
-    contract = "counter",
+    contract = "counter_2",
     name = "increment",
     parameter = "IncrementVal",
     error = "Error",
@@ -52,7 +52,7 @@ fn increment(ctx: &ReceiveContext, host: &mut Host<State>) -> Result<(), Error> 
     Ok(())
 }
 
-#[receive(contract = "counter", name = "decrement", parameter = "IncrementVal", error = "Error", mutable)]
+#[receive(contract = "counter_2", name = "decrement", parameter = "IncrementVal", error = "Error", mutable)]
 fn decrement(ctx: &ReceiveContext, host: &mut Host<State>) -> Result<(), Error> {
     let param :IncrementVal = ctx.parameter_cursor().get()?;
     let state = host.state_mut();
@@ -62,7 +62,13 @@ fn decrement(ctx: &ReceiveContext, host: &mut Host<State>) -> Result<(), Error> 
     Ok(())
 }
 
-#[receive(contract = "counter", name = "view", return_value = "i8")]
+#[receive(contract = "counter_2", name = "view", return_value = "i8")]
 fn view<'a, 'b>(_ctx: &'a ReceiveContext, host: &'b Host<State>) -> ReceiveResult<i8> {
     Ok(host.state().counter)
 }
+
+
+//concordium-client account show ACCOUNT --grpc-port 20000 --grpc-ip node.testnet.concordium.com
+//Module reference d43fd5c0e4e557077a89d9aff9186158f9fbecec38d6b0c05df28470e4818d0b was successfully named 'counter_1'.
+//Module reference 52f60c20223b668910cea6959ef4e551ea8092288b29cd99000784e580cbd5bb was successfully named 'counter_2'.
+//Contract successfully initialized with address: {"index":7374,"subindex":0}
